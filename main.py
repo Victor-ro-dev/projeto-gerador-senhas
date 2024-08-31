788c5bc8473f7303052575a388f4f5bac14eeca7
Victor Rodrigues de Oliveira <victor.ro.dev10@gmail.com>
2024-08-31 14:07:50 -0300

    criando janela

--- main.py
+++ main.py
@@ -5,3 +5,23 @@ from tkinter import messagebox
 from PIL import ImageTk, Image
 import string
 import random
+
+cor1 = "#0a0a0a"  # Preta
+cor2 = "#fafcff"  # Branca
+cor3 = "#21c25c"  # Verde
+cor4 = "#eb463b"  # Vermelha
+cor5 = "#dedcdc"  # Cinza
+cor6 = "#3080f0"  # Azul
+
+janela = Tk()
+janela.geometry("300x350")
+janela.config(bg=cor2)
+janela.resizable(width=False, height=False)
+
+
+
+
+
+estilo = ttk.Style(janela)
+estilo.theme_use('clam')
+janela.mainloop()
